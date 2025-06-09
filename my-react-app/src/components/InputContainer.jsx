58f8b3cd33bf6815660e13cb8bf7d7dc130602e6
akhilagit <akhilabojja2@gmail.com>
2025-06-09 12:34:24 +0530

    Configure GitHub Pages deployment

--- my-react-app/src/components/InputContainer.jsx
+++ my-react-app/src/components/InputContainer.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function InputContainer({inputValue,writeToDo,addTodo}) {
+function InputContainer({ inputValue, writeToDo, addTodo }) {
   return (
-     <div className="input-container">
-        <input type="text" placeholder="Enter a Task" value={inputValue} onChange={writeToDo} />
-        <button onClick={addTodo}>+</button>
-      </div>
+    <div className="input-container">
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={inputValue}
+        onChange={writeToDo}
+      />
+      <button onClick={addTodo}>+</button>
+    </div>
   );
 }
 export default InputContainer;
